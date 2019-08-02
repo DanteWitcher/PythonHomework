850aae2dcd5327b6a972cfc1993d667e8453d577
aliaksandr <kakhanovichal@gmail.com>
2019-08-02 20:03:50 +0300

    chore: boilerplate

---
+++ final_task/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='calculator',
+    version='0.1.0',
+    packages=['calculator'],
+    url='',
+    license='MIT',
+    author='aliaksandr kakhanovich',
+    author_email='kakhanovichal@gmail.com',
+    description='simple calculator',
+    scripts=['bin/pycalc']
+)
